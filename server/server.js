464300f78078f6af24b70e95b4f7aebc59b39a45
Dragon Huang <j0918023423@gmail.com>
2024-12-01 18:34:45 +0800

    refactor: remove old code

--- server/server.js
+++ server/server.js
@@ -5,15 +5,6 @@ const port = process.env.PORT || 5000;
 
 app.post('/v1/users', routes.createUser);
 
-// app.get('/', routes.getAllTodos);
-// app.get('/:id', routes.getTodo);
-
-// app.post('/', routes.postTodo);
-// app.patch('/:id', routes.patchTodo);
-
-// app.delete('/', routes.deleteAllTodos);
-// app.delete('/:id', routes.deleteTodo);
-
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
