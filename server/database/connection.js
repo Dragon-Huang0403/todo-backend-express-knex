2adc79d92b31622307894f296ac697e9ba1e6354
Dragon Huang <j0918023423@gmail.com>
2024-12-03 19:59:51 +0800

    refactor: move knex config into db connection

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,4 +1,21 @@
-// Abstraction layer to handle knex configuration per enviornment.
-const config = require('../knexfile.js');
+const config = require('../config/config.js');
 
-module.exports = require('knex')(config);
+const knexConfig = {
+  client: 'postgresql',
+  connection: {
+    host: config.PG_HOST,
+    port: config.PG_PORT,
+    user: config.PG_USER,
+    password: config.PG_PASSWORD,
+    database: config.PG_DATABASE,
+  },
+  pool: {
+    min: config.PG_POOL_MIN,
+    max: config.PG_POOL_MAX,
+  },
+  migrations: {
+    tableName: config.PG_MIGRATION_TABLE_NAME,
+  },
+};
+
+module.exports = require('knex')(knexConfig);
